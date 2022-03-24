28b71b601b17b7d5d712d729247f10e9a1775824
chad moore <thryce85@tamu.edu>
2022-03-24 17:16:44 -0500

    add to cart finished

--- class-14/lab/starter-code/js/app.js
+++ class-14/lab/starter-code/js/app.js
@@ -8,7 +8,7 @@ const Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
-  this.items= new CartItem(product, quantity) ;
+  this.items.push( new CartItem(product, quantity) );
   // console.log('in cart items');
 };
 
